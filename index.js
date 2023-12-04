002066a6d6d033233a5172419b20593fbb7df94c
allamemc <allamemc@gmail.com>
2023-12-05 00:12:03 +0100

    Update server console log message

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port PORT`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
